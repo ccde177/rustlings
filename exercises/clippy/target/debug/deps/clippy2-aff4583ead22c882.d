3fa62986dd5e281714b83bc35603ba0f10b968be
ccde177b5fb9b8c55078417b4c9fee <sammorow@gmail.com>
2021-06-29 12:07:29 +0300

    Solutions upload

---
+++ exercises/clippy/target/debug/deps/clippy2-aff4583ead22c882.d
@@ -0,0 +1,7 @@
+/home/user/pkg/rustlings/exercises/clippy/target/debug/deps/clippy2-aff4583ead22c882.rmeta: clippy2.rs
+
+/home/user/pkg/rustlings/exercises/clippy/target/debug/deps/clippy2-aff4583ead22c882.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
